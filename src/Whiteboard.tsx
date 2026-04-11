318e9239a17d6f3da7d7e73fb3d052590baf251a
Praise Olatope <aanuolatope@gmail.com>
2026-04-10 20:22:20 -0600

    Added Whiteboard Screen

--- src/Whiteboard.tsx
+++ src/Whiteboard.tsx
@@ -1,10 +1,47 @@
-import { useParams } from 'react-router'
+import { CheckCircle, PencilSimple, Spinner } from "phosphor-react";
+import { useState } from "react";
+import { useParams } from "react-router";
 
 const WhiteboardScreen = () => {
     const { id } = useParams();
-  return (
-    <div>WhiteboardScreenID: {id}</div>
-  )
-}
+    const [Saved, SetSaved] = useState(false);
 
-export default WhiteboardScreen
+    function ChangeSaved() {
+        SetSaved(Saved ? false : true);
+    }
+    return (
+        <div className="Whiteboard-Area">
+            <div className="Whiteboard-Top">
+                <input defaultValue={id} className="Whiteboard-Name"></input>
+                <div>
+                    {Saved ? (
+                        <CheckCircle
+                            color="Green"
+                            weight="bold"
+                            size={20}
+                        ></CheckCircle>
+                    ) : (
+                        <Spinner
+                            color="Grey"
+                            weight="bold"
+                            size={20}
+                            className="Spin"
+                        ></Spinner>
+                    )}
+                </div>
+            </div>
+
+            <div className="Whiteboard-Options">
+                <div className="Whiteboard-Option">
+                    <PencilSimple
+                        weight="bold"
+                        size={32}
+                        className="Dark"
+                    ></PencilSimple>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default WhiteboardScreen;
